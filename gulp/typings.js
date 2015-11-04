851b1942f43c222a6cba27bb98548869203be250
Sebastian Müller <info@sebastian-mueller.net>
2015-11-04 11:01:29 +0100

    chore(typings): fix modules
    To support multiple main modules in the future, we fix the .d.ts with a
    crude hack: angular2-google-maps -> angular2-google-maps/angular2-google-maps
    
    In the future, there will be multiple modules like:
    
    angular2-google-maps/components
    angular2-google-maps/components/my-comp

--- gulp/typings.js
+++ gulp/typings.js
@@ -2,11 +2,25 @@ const gulp = require('gulp');
 const config = require('./config');
 const path = require('path');
 const dts = require('dts-bundle');
+const replace = require('replace');
 
-gulp.task('typings', ['scripts:es5'], function typings() {
+gulp.task('typings', ['scripts:es5'], function typings(done) {
+  const outFile = path.join(__dirname, '../', config.PATHS.dist.typings, 'angular2-google-maps.d.ts');
+
   dts.bundle({
     name: 'angular2-google-maps',
-    main: path.join(config.PATHS.dist.es5, 'test.d.ts'),
-    out: path.join(__dirname, '../', config.PATHS.dist.typings, 'angular2-google-maps.d.ts'),
+    indent: ' ',
+    prefix: '',
+    main: path.join(config.PATHS.dist.es5, 'angular2-google-maps.d.ts'),
+    out: outFile,
   });
+
+  replace({
+    paths: [outFile],
+    regex: 'declare module \'angular2-google-maps\'',
+    replacement: 'declare module \'angular2-google-maps/angular2-google-maps\'',
+    silent: true,
+  });
+
+  done();
 });
--- typings.d.ts
+++ typings.d.ts
@@ -1,1 +1,2 @@
-/// <reference path="node_modules/angular2/angular2.d.ts" />
+/// <reference path="node_modules/angular2/bundles/typings/angular2/angular2.d.ts" />
+/// <reference path="dist/typings/angular2-google-maps.d.ts" />
