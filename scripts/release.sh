93063daf5976c27bb413d535876f17272183e408
Sebastian Müller <info@sebastian-mueller.net>
2015-11-19 21:42:20 +0100

    chore(scripts/release): remove github-release

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-# PLEASE NOTE - make sure that you have set up these things before running this script!!: 
+# PLEASE NOTE - make sure that you have set up these things before running this script!!:
 #  - bump the package.json version
 #  - generate the project with gulp build (dist folder should lookin good)
-#  - ANGULAR2_GOOGLE_MAPS_TOKEN enviroment variable should be set with your Github token
+#  - Create Git tag
 ./node_modules/.bin/gulp changelog
 git add CHANGELOG.md package.json
-git commit -m "chore(release): changelog/version bump"
+git commit -m "chore(release): add changelog and version"
 git push origin master
 ./node_modules/.bin/gulp create-tag
 git push origin master --tags
 ./node_modules/.bin/gulp github-release
-echo "--- DONE RELEASING --- Please publish to NPM now"
+echo "--- DONE RELEASING --- Please publish to NPM now (run npm publish dist)"
