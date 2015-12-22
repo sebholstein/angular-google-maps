c12b04a555b147006da5b4cd29cc3850ed2d5b90
Sebastian Müller <info@sebastian-mueller.net>
2015-12-22 17:45:24 +0100

    chore(gulp): remove typings task in watch

--- gulp/watch.js
+++ gulp/watch.js
@@ -2,5 +2,5 @@ const gulp = require('gulp');
 const config = require('./config');
 
 gulp.task('watch', function watch() {
-  gulp.watch(config.PATHS.tsSrcFiles, ['scripts:cjs', 'bundle:cjs', 'typings']);
+  gulp.watch(config.PATHS.tsSrcFiles, ['scripts:cjs', 'bundle:cjs']);
 });
