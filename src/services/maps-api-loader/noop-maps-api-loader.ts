a94c7e98e61a064731b56181089e3ef43271c3c7
Sebastian Müller <info@sebastian-mueller.net>
2016-01-10 21:44:43 +0100

    chore(*): changes for docs generation

--- src/services/maps-api-loader/noop-maps-api-loader.ts
+++ src/services/maps-api-loader/noop-maps-api-loader.ts
@@ -1,7 +1,7 @@
 import {MapsAPILoader} from './maps-api-loader';
 
 /**
- * When using the NoOpMapsAPILoader, the Google Maps API must be added to the page via a <script>
+ * When using the NoOpMapsAPILoader, the Google Maps API must be added to the page via a `<script>`
  * Tag.
  * It's important that the Google Maps API script gets loaded first on the page.
  */
