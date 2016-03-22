7c95e55c07e4dee8bcdfef3adffa923ff3724854
Sebastian Müller <info@sebastian-mueller.net>
2016-03-22 22:03:59 +0100

    feat(*): Expose InfoWindowManager

--- src/services.ts
+++ src/services.ts
@@ -2,6 +2,7 @@ export {MapsAPILoader} from './services/maps-api-loader/maps-api-loader';
 export {NoOpMapsAPILoader} from './services/maps-api-loader/noop-maps-api-loader';
 export {GoogleMapsAPIWrapper} from './services/google-maps-api-wrapper';
 export {MarkerManager} from './services/marker-manager';
+export {InfoWindowManager} from './services/info-window-manager';
 export {
   LazyMapsAPILoader,
   LazyMapsAPILoaderConfig,
