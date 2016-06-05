3faa0b940d4e1d7c642b1aafd87a4b7293a1769c
Sebastian Müller <info@sebastian-mueller.net>
2016-06-05 20:57:24 +0200

    chore(clang): format code for new rules

--- src/directives-const.ts
+++ src/directives-const.ts
@@ -1,6 +1,6 @@
 import {SebmGoogleMap} from './directives/google-map';
-import {SebmGoogleMapMarker} from './directives/google-map-marker';
 import {SebmGoogleMapInfoWindow} from './directives/google-map-info-window';
+import {SebmGoogleMapMarker} from './directives/google-map-marker';
 
 export const ANGULAR2_GOOGLE_MAPS_DIRECTIVES: any[] =
     [SebmGoogleMap, SebmGoogleMapMarker, SebmGoogleMapInfoWindow];
--- src/services.ts
+++ src/services.ts
@@ -1,6 +1,6 @@
-export {MapsAPILoader} from './services/maps-api-loader/maps-api-loader';
-export {NoOpMapsAPILoader} from './services/maps-api-loader/noop-maps-api-loader';
 export {GoogleMapsAPIWrapper} from './services/google-maps-api-wrapper';
-export {MarkerManager} from './services/marker-manager';
 export {InfoWindowManager} from './services/info-window-manager';
 export {LazyMapsAPILoader, LazyMapsAPILoaderConfig, GoogleMapsScriptProtocol} from './services/maps-api-loader/lazy-maps-api-loader';
+export {MapsAPILoader} from './services/maps-api-loader/maps-api-loader';
+export {NoOpMapsAPILoader} from './services/maps-api-loader/noop-maps-api-loader';
+export {MarkerManager} from './services/marker-manager';
