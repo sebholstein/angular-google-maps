3ead15e68ac7967eace833be15b53e40a3b021e5
Sebastian Müller <info@sebastian-mueller.net>
2016-06-08 23:25:52 +0200

    feat(*): rename ANGULAR2_GOOGLE_MAPS_* constants
    This removes the ANGULAR2_ prefix.
    
    BREAKING CHANGES
    
    `ANGULAR2_GOOGLE_MAPS_PROVIDERS` is now called `GOOGLE_MAPS_PROVIDERS`.
    `ANGULAR2_GOOGLE_MAPS_DIRECTIVES`is now called `GOOGLE_MAPS_DIRECTIVES`.
    
    Please update existing import of these two constants in your code.
    
    Related to #395

--- src/core/directives-const.ts
+++ src/core/directives-const.ts
@@ -2,5 +2,5 @@ import {SebmGoogleMap} from './directives/google-map';
 import {SebmGoogleMapInfoWindow} from './directives/google-map-info-window';
 import {SebmGoogleMapMarker} from './directives/google-map-marker';
 
-export const ANGULAR2_GOOGLE_MAPS_DIRECTIVES: any[] =
+export const GOOGLE_MAPS_DIRECTIVES: any[] =
     [SebmGoogleMap, SebmGoogleMapMarker, SebmGoogleMapInfoWindow];
