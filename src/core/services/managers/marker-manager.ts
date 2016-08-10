75f37a62a9f87c5ac66bf59e89a3e320515999f1
TSHiYK <toshiyuki.i1223+dev@gmail.com>
2016-08-11 07:04:47 +0900

    feat(SebmGoogleMapMarker): support zIndex option
    Closes #559
    Closes #558

--- src/core/services/managers/marker-manager.ts
+++ src/core/services/managers/marker-manager.ts
@@ -57,6 +57,10 @@ export class MarkerManager {
     return this._markers.get(marker).then((m: Marker) => m.setVisible(marker.visible));
   }
 
+  updateZIndex(marker: SebmGoogleMapMarker): Promise<void> {
+    return this._markers.get(marker).then((m: Marker) => m.setZIndex(marker.zIndex));
+  }
+
   addMarker(marker: SebmGoogleMapMarker) {
     const markerPromise = this._mapsWrapper.createMarker({
       position: {lat: marker.latitude, lng: marker.longitude},
@@ -64,7 +68,8 @@ export class MarkerManager {
       draggable: marker.draggable,
       icon: marker.iconUrl,
       opacity: marker.opacity,
-      visible: marker.visible
+      visible: marker.visible,
+      zIndex: marker.zIndex
     });
     this._markers.set(marker, markerPromise);
   }
