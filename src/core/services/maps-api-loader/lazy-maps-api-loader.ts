254221daa834504fd4d681fa98f13776a88537f4
Tempus35 <dtime35@gmail.com>
2016-09-01 14:16:04 -0700

    feat(*): Fix some issues

--- src/core/services/maps-api-loader/lazy-maps-api-loader.ts
+++ src/core/services/maps-api-loader/lazy-maps-api-loader.ts
@@ -1,9 +1,5 @@
-/**
- * angular2-google-maps - Angular 2 components for Google Maps
- * @version v0.13.0
- * @link https://github.com/SebastianM/angular2-google-maps#readme
- * @license MIT
- */
+import {Inject, Injectable, Optional, Provider} from '@angular/core';
+
 import {DOCUMENT_GLOBAL, WINDOW_GLOBAL} from '../../utils/browser-globals';
 
 import {MapsAPILoader} from './maps-api-loader';
--- src/core/utils/browser-globals.ts
+++ src/core/utils/browser-globals.ts
@@ -1,9 +1,5 @@
-/**
- * angular2-google-maps - Angular 2 components for Google Maps
- * @version v0.13.0
- * @link https://github.com/SebastianM/angular2-google-maps#readme
- * @license MIT
- */
+import {OpaqueToken, Provider} from '@angular/core';
+
 export const WINDOW_GLOBAL = new OpaqueToken('angular2-google-maps window_global');
 export const DOCUMENT_GLOBAL = new OpaqueToken('angular2-google-maps document_global');
 
