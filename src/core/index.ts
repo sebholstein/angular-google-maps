d3a975eb2dffdaf79bc9a2c017a046216e119d8a
Sebastian Müller <info@sebastian-mueller.net>
2016-09-01 21:04:15 +0200

    refactor(core): rm GOOGLE_MAPS_DIRECTIVES
    BREAKING CHANGE: previously deprecated GOOGLE_MAPS_DIRECTIVES
    was removed. Please use AgmCoreModule instead.

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,6 +1,11 @@
 import {ModuleWithProviders, NgModule, provide} from '@angular/core';
 
-import {GOOGLE_MAPS_DIRECTIVES} from './directives-const';
+import {SebmGoogleMap} from './directives/google-map';
+import {SebmGoogleMapCircle} from './directives/google-map-circle';
+import {SebmGoogleMapInfoWindow} from './directives/google-map-info-window';
+import {SebmGoogleMapMarker} from './directives/google-map-marker';
+import {SebmGoogleMapPolyline} from './directives/google-map-polyline';
+import {SebmGoogleMapPolylinePoint} from './directives/google-map-polyline-point';
 import {LazyMapsAPILoader} from './services/maps-api-loader/lazy-maps-api-loader';
 import {MapsAPILoader} from './services/maps-api-loader/maps-api-loader';
 import {BROWSER_GLOBALS_PROVIDERS} from './utils/browser-globals';
@@ -13,17 +18,21 @@ export * from './map-types';
 // Google Maps types
 export {LatLngBounds, LatLng, LatLngLiteral, MapTypeStyle} from './services/google-maps-types';
 
-/** @deprecated */
 const GOOGLE_MAPS_PROVIDERS: any[] = [
   BROWSER_GLOBALS_PROVIDERS,
   provide(MapsAPILoader, {useClass: LazyMapsAPILoader}),
+];
+
+const CORE_DIRECTIVES: any[] = [
+  SebmGoogleMap, SebmGoogleMapMarker, SebmGoogleMapInfoWindow, SebmGoogleMapCircle,
+  SebmGoogleMapPolyline, SebmGoogleMapPolylinePoint
 ];
 
 /**
  * The angular2-google-maps core module. Contains all Directives/Services/Pipes
  * of the core module. Please use `AgmCoreModule.forRoot()` in your app module.
  */
-@NgModule({declarations: GOOGLE_MAPS_DIRECTIVES, exports: GOOGLE_MAPS_DIRECTIVES})
+@NgModule({declarations: CORE_DIRECTIVES, exports: CORE_DIRECTIVES})
 export class AgmCoreModule {
   static forRoot(): ModuleWithProviders {
     return {
