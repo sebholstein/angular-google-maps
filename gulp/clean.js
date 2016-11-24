8416d875c7bf03b20c708f427806e210d5b52607
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2016-11-24 20:52:26 +0100

    chore(): remove obsolete gulp files

--- gulp/clean.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const del = require('del');
-const config = require('./config');
-
-gulp.task('clean:dist', (done) => del(config.PATHS.dist.base, done));
-
-gulp.task('clean:test', (done) => del(config.PATHS.testBuilt, done));
-
-gulp.task('clean:tmp', (done) => del(config.PATHS.tmp, done));
