52eae2f84e5c580b5ddfde12c53c90f4e26a3040
Sebastian Müller <info@sebastian-mueller.net>
2017-04-09 20:10:52 +0200

    feat: change name to AGM - Angular Google Maps

--- scripts/create-package-json.js
+++ scripts/create-package-json.js
@@ -10,16 +10,14 @@ pkgNames.forEach(function(pkgName) {
   const basePkgJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
   // define the package name
-  basePkgJson.name = `@angular-google-maps/${pkgName}`
+  basePkgJson.name = `@agm/${pkgName}`
 
   // remove scripts
   delete basePkgJson.scripts;
 
   // remove devDependencies (as there are important for the sourcecode only)
   delete basePkgJson.devDependencies;
-
-  // transform dependencies to peerDependencies for the release
-  basePkgJson.peerDependencies = Object.assign({}, basePkgJson.dependencies);
+  
   basePkgJson.dependencies = {};
 
   const filepath = path.join(__dirname, `../dist/${pkgName}/package.json`);
