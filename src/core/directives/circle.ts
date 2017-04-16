e4ce9dd72ad2948e9a8393e079200bf7f05ac30f
Sebastian Müller <info@sebastian-mueller.net>
2017-04-16 09:42:45 +0200

    refactor(AgmCircle): use input/outputs

--- src/core/directives/circle.ts
+++ src/core/directives/circle.ts
@@ -1,4 +1,4 @@
-import {Directive, EventEmitter, OnChanges, OnDestroy, OnInit, SimpleChange} from '@angular/core';
+import {Directive, EventEmitter, OnChanges, OnDestroy, OnInit, SimpleChange, Input, Output} from '@angular/core';
 import {Subscription} from 'rxjs/Subscription';
 
 import {MouseEvent} from '../map-types';
@@ -7,154 +7,146 @@ import {MouseEvent as MapMouseEvent} from '../services/google-maps-types';
 import {CircleManager} from '../services/managers/circle-manager';
 
 @Directive({
-  selector: 'agm-circle',
-  inputs: [
-    'latitude', 'longitude', 'clickable', 'draggable: circleDraggable', 'editable', 'fillColor',
-    'fillOpacity', 'radius', 'strokeColor', 'strokeOpacity', 'strokePosition', 'strokeWeight',
-    'visible', 'zIndex'
-  ],
-  outputs: [
-    'centerChange', 'circleClick', 'circleDblClick', 'drag', 'dragEnd', 'dragStart', 'mouseDown',
-    'mouseMove', 'mouseOut', 'mouseOver', 'mouseUp', 'radiusChange', 'rightClick'
-  ]
+  selector: 'agm-circle'
 })
 export class AgmCircle implements OnInit, OnChanges, OnDestroy {
   /**
    * The latitude position of the circle (required).
    */
-  latitude: number;
+  @Input() latitude: number;
 
   /**
    * The clickable position of the circle (required).
    */
-  longitude: number;
+  @Input() longitude: number;
 
   /**
    * Indicates whether this Circle handles mouse events. Defaults to true.
    */
-  clickable: boolean = true;
+  @Input() clickable: boolean = true;
 
   /**
    * If set to true, the user can drag this circle over the map. Defaults to false.
    */
-  draggable: boolean = false;
+  // tslint:disable-next-line:no-input-rename
+  @Input('circleDraggable') draggable: boolean = false;
 
   /**
    * If set to true, the user can edit this circle by dragging the control points shown at
    * the center and around the circumference of the circle. Defaults to false.
    */
-  editable: boolean = false;
+  @Input() editable: boolean = false;
 
   /**
    * The fill color. All CSS3 colors are supported except for extended named colors.
    */
-  fillColor: string;
+  @Input() fillColor: string;
 
   /**
    * The fill opacity between 0.0 and 1.0.
    */
-  fillOpacity: number;
+  @Input() fillOpacity: number;
 
   /**
    * The radius in meters on the Earth's surface.
    */
-  radius: number = 0;
+  @Input() radius: number = 0;
 
   /**
    * The stroke color. All CSS3 colors are supported except for extended named colors.
    */
-  strokeColor: string;
+  @Input() strokeColor: string;
 
   /**
    * The stroke opacity between 0.0 and 1.0
    */
-  strokeOpacity: number;
+  @Input() strokeOpacity: number;
 
   /**
    * The stroke position. Defaults to CENTER.
    * This property is not supported on Internet Explorer 8 and earlier.
    */
-  strokePosition: 'CENTER'|'INSIDE'|'OUTSIDE' = 'CENTER';
+  @Input() strokePosition: 'CENTER'|'INSIDE'|'OUTSIDE' = 'CENTER';
 
   /**
    * The stroke width in pixels.
    */
-  strokeWeight: number = 0;
+  @Input() strokeWeight: number = 0;
 
   /**
    * Whether this circle is visible on the map. Defaults to true.
    */
-  visible: boolean = true;
+  @Input() visible: boolean = true;
 
   /**
    * The zIndex compared to other polys.
    */
-  zIndex: number;
+  @Input() zIndex: number;
 
   /**
    * This event is fired when the circle's center is changed.
    */
-  centerChange: EventEmitter<LatLngLiteral> = new EventEmitter<LatLngLiteral>();
+  @Output() centerChange: EventEmitter<LatLngLiteral> = new EventEmitter<LatLngLiteral>();
 
   /**
    * This event emitter gets emitted when the user clicks on the circle.
    */
-  circleClick: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
+  @Output() circleClick: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
 
   /**
    * This event emitter gets emitted when the user clicks on the circle.
    */
-  circleDblClick: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
+  @Output() circleDblClick: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
 
   /**
    * This event is repeatedly fired while the user drags the circle.
    */
-  drag: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
+  @Output() drag: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
 
   /**
    * This event is fired when the user stops dragging the circle.
    */
-  dragEnd: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
+  @Output() dragEnd: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
 
   /**
    * This event is fired when the user starts dragging the circle.
    */
-  dragStart: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
+  @Output() dragStart: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
 
   /**
    * This event is fired when the DOM mousedown event is fired on the circle.
    */
-  mouseDown: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
+  @Output() mouseDown: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
 
   /**
    * This event is fired when the DOM mousemove event is fired on the circle.
    */
-  mouseMove: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
+  @Output() mouseMove: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
 
   /**
    * This event is fired on circle mouseout.
    */
-  mouseOut: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
+  @Output() mouseOut: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
 
   /**
    * This event is fired on circle mouseover.
    */
-  mouseOver: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
+  @Output() mouseOver: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
 
   /**
    * This event is fired when the DOM mouseup event is fired on the circle.
    */
-  mouseUp: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
+  @Output() mouseUp: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
 
   /**
    * This event is fired when the circle's radius is changed.
    */
-  radiusChange: EventEmitter<number> = new EventEmitter<number>();
+  @Output() radiusChange: EventEmitter<number> = new EventEmitter<number>();
 
   /**
    * This event is fired when the circle is right-clicked on.
    */
-  rightClick: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
+  @Output() rightClick: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
 
   private _circleAddedToManager: boolean = false;
 
