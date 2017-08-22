b108ef9acac5fd9c2838883c5074061f24f2e632
Sebastian Müller <info@sebastian-mueller.net>
2017-08-22 20:32:55 +0200

    chore: fix snazzy-info-window rollup config

--- rollup.snazzy-info-window.config.js
+++ rollup.snazzy-info-window.config.js
@@ -18,5 +18,5 @@ export default {
     '@agm/core': 'ngmaps.core'
   },
   context: 'window',
-  external: ['rxjs', '@angular/core', 'rxjs/Observable']
+  external: ['rxjs', '@angular/core', 'rxjs/Observable', '@agm/core']
 }
