1a7193b3cc396fa6d323a7ab0d37789728c19c24
Seany Computer <sean@wiredviews.com>
2017-09-14 16:30:38 -0400

    fix(lazy-maps-api-loader): 'const' initializer in an ambient context must be a string or numeric literal typescript 2.4 error

--- packages/core/services/maps-api-loader/lazy-maps-api-loader.ts
+++ packages/core/services/maps-api-loader/lazy-maps-api-loader.ts
@@ -14,7 +14,7 @@ export enum GoogleMapsScriptProtocol {
  * Token for the config of the LazyMapsAPILoader. Please provide an object of type {@link
  * LazyMapsAPILoaderConfig}.
  */
-export const LAZY_MAPS_API_CONFIG = new InjectionToken('angular-google-maps LAZY_MAPS_API_CONFIG');
+export declare const LAZY_MAPS_API_CONFIG: InjectionToken<LazyMapsAPILoaderConfigLiteral>;
 
 /**
  * Configuration for the {@link LazyMapsAPILoader}.
