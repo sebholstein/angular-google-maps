9f31dcb16b7ea9f1164a858885e8be50a83783d1
INT\Gleb.Popov <gleb.popov@nordigy.ru>
2018-02-02 19:26:44 +0300

    added marker label interface

--- packages/core/map-types.ts
+++ packages/core/map-types.ts
@@ -1,7 +1,15 @@
 import {LatLngLiteral} from './services/google-maps-types';
 
 // exported map types
-export {KmlMouseEvent, DataMouseEvent, LatLngBounds, LatLngBoundsLiteral, LatLngLiteral, PolyMouseEvent} from './services/google-maps-types';
+export {
+  KmlMouseEvent,
+  DataMouseEvent,
+  LatLngBounds,
+  LatLngBoundsLiteral,
+  LatLngLiteral,
+  PolyMouseEvent,
+  MarkerLabel
+} from './services/google-maps-types';
 
 /**
  * MouseEvent gets emitted when the user triggers mouse events on the map.
