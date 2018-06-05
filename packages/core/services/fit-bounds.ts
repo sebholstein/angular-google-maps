c649f70f88ce9ce1245920cd3e9a49e0b066a0dc
Sebastian Holstein <info@sebastian-mueller.net>
2018-06-05 22:26:47 +0200

    add tests

--- packages/core/services/fit-bounds.ts
+++ packages/core/services/fit-bounds.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, from } from 'rxjs';
-import { flatMap, map, skipWhile } from 'rxjs/operators';
+import { BehaviorSubject, Observable, from, timer } from 'rxjs';
+import {
+  flatMap,
+  map,
+  skipWhile,
+  sample,
+  switchMap,
+  shareReplay
+} from 'rxjs/operators';
 import { LatLng, LatLngBounds, LatLngLiteral } from './google-maps-types';
 import { MapsAPILoader } from './maps-api-loader/maps-api-loader';
 
 declare var google: any;
 
+// todo: internal?
 export interface FitBoundsDetails {
-  latLng: LatLng|LatLngLiteral;
+  latLng: LatLng | LatLngLiteral;
 }
+
+/**
+ * @internal
+ */
+export type BoundsMap = Map<string, LatLng | LatLngLiteral>;
 
 export abstract class FitBoundsAccessor {
   abstract getFitBoundsDetails$(): Observable<FitBoundsDetails>;
@@ -16,22 +29,27 @@ export abstract class FitBoundsAccessor {
 
 @Injectable()
 export class FitBoundsService {
-  readonly bounds$: Observable<LatLngBounds>;
-  protected readonly _boundsChangeDebounceTime$: BehaviorSubject<number> = new BehaviorSubject<number>(200);
-  protected readonly _includeInBounds$: BehaviorSubject<Map<string, LatLng | LatLngLiteral>> = new BehaviorSubject<Map<string, LatLng | LatLngLiteral>>(new Map<string, LatLng | LatLngLiteral>());
-  protected _emitPaused: boolean = false;
+  protected readonly bounds$: Observable<LatLngBounds>;
+  protected readonly _boundsChangeSampleTime$ = new BehaviorSubject<number>(200);
+  protected readonly _includeInBounds$ = new BehaviorSubject<BoundsMap>(new Map<string, LatLng | LatLngLiteral>());
 
   constructor(loader: MapsAPILoader) {
     this.bounds$ = from(loader.load()).pipe(
       flatMap(() => this._includeInBounds$),
-      skipWhile(() => this._emitPaused),
-      // debounce(() => this._boundsChangeDebounceTime$),
-      map((includeInBounds: Map<string, LatLng | LatLngLiteral>) => {
-        const bounds = new google.maps.LatLngBounds() as LatLngBounds;
-        includeInBounds.forEach(b => bounds.extend(b));
-        return bounds;
-      })
+      sample(
+        this._boundsChangeSampleTime$.pipe(switchMap(time => timer(0, time)))
+      ),
+      map(includeInBounds => this._generateBounds(includeInBounds)),
+      shareReplay(1)
     );
+  }
+
+  private _generateBounds(
+    includeInBounds: Map<string, LatLng | LatLngLiteral>
+  ) {
+    const bounds = new google.maps.LatLngBounds() as LatLngBounds;
+    includeInBounds.forEach(b => bounds.extend(b));
+    return bounds;
   }
 
   addToBounds(latLng: LatLng | LatLngLiteral) {
@@ -44,17 +62,21 @@ export class FitBoundsService {
     this._includeInBounds$.next(map);
   }
 
-  removeFromBounds(latLng: LatLng|LatLngLiteral) {
+  removeFromBounds(latLng: LatLng | LatLngLiteral) {
     const map = this._includeInBounds$.value;
     map.delete(this._createIdentifier(latLng));
     this._includeInBounds$.next(map);
   }
 
-  changeFitBoundsDebounceTime(timeMs: number) {
-    this._boundsChangeDebounceTime$.next(timeMs);
+  changeFitBoundsChangeSampleTime(timeMs: number) {
+    this._boundsChangeSampleTime$.next(timeMs);
   }
 
-  protected _createIdentifier(latLng: LatLng|LatLngLiteral): string {
+  getBounds$(): Observable<LatLngBounds> {
+    return this.bounds$;
+  }
+
+  protected _createIdentifier(latLng: LatLng | LatLngLiteral): string {
     return `${latLng.lat}+${latLng.lng}`;
   }
 }
