1f2724b9e5d39fa99dc00d0204945eebd8d443b4
Terence Honles <terence@honles.com>
2019-09-17 03:15:27 -0700

    feat(MarkerClusterer): enable clusterclick event
    Add clusterClick event to MarkerCluster.
    
    fixes: #1272

--- packages/js-marker-clusterer/directives/marker-cluster.ts
+++ packages/js-marker-clusterer/directives/marker-cluster.ts
@@ -1,9 +1,11 @@
-import { Directive, Input, OnChanges, OnDestroy, OnInit, SimpleChange } from '@angular/core';
+import { Directive, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChange } from '@angular/core';
 
 import { InfoWindowManager, MarkerManager } from '@agm/core';
 import { ClusterManager } from '../services/managers/cluster-manager';
 
 import { CalculateFunction, ClusterOptions, ClusterStyle } from '../services/google-clusterer-types';
+
+import { Subscription } from 'rxjs';
 
 /**
  * AgmMarkerCluster clusters map marker if they are near together
@@ -79,11 +81,15 @@ export class AgmMarkerCluster implements OnDestroy, OnChanges, OnInit, ClusterOp
   @Input() imagePath: string;
   @Input() imageExtension: string;
 
+  @Output() clusterClick: EventEmitter<void> = new EventEmitter<void>();
+
+  private _observableSubscriptions: Subscription[] = [];
   constructor(private _clusterManager: ClusterManager) { }
 
   /** @internal */
   ngOnDestroy() {
     this._clusterManager.clearMarkers();
+    this._observableSubscriptions.forEach((s) => s.unsubscribe());
   }
 
   /** @internal */
@@ -117,8 +123,22 @@ export class AgmMarkerCluster implements OnDestroy, OnChanges, OnInit, ClusterOp
     }
   }
 
+  private _addEventListeners() {
+    const handlers = [
+      {
+        name: 'clusterclick',
+        handler: () => this.clusterClick.emit(),
+      },
+    ];
+    handlers.forEach((obj) => {
+      const os = this._clusterManager.createClusterEventObservable(obj.name).subscribe(obj.handler);
+      this._observableSubscriptions.push(os);
+    });
+  }
+
   /** @internal */
   ngOnInit() {
+    this._addEventListeners();
     this._clusterManager.init({
       gridSize: this.gridSize,
       maxZoom: this.maxZoom,
