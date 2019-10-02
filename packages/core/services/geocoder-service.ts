f1b985cb92eae2991b66aa1f40b30a2a0e45dc25
hero2frag@gmail.com <hero2frag@gmail.com>
2019-10-02 21:19:15 +0200

    feat(AgmGeocoder): add geocoder service
    - add geocoder object as private observable
    to follow style of other services
    
    Implements #1694

--- packages/core/services/geocoder-service.ts
+++ packages/core/services/geocoder-service.ts
@@ -1,22 +1,27 @@
 import { Injectable } from '@angular/core';
-import { of, throwError, from } from 'rxjs';
-import { GeocoderRequest, GeocoderStatus, google, Geocoder } from './google-maps-types';
+import { of, throwError, from, Observable } from 'rxjs';
+import { GeocoderRequest, GeocoderStatus, google, Geocoder, GeocoderResult } from './google-maps-types';
 import { MapsAPILoader } from './maps-api-loader/maps-api-loader';
 import { WindowRef } from '../utils/browser-globals';
 import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AgmGeocoder {
+  private geocoder$: Observable<Geocoder>;
   protected _windowRef: WindowRef;
 
-  constructor(private loader: MapsAPILoader) { }
+  constructor(private loader: MapsAPILoader) {
+    this.geocoder$ = from(loader.load()).pipe(
+      map(() => {
+        return new google.maps.Geocoder() as Geocoder;
+      })
+    )
+  }
 
 
   geocode(request: GeocoderRequest) {
-    return from(this.loader.load()).pipe(
-      map(() => {
-        const geocoder = new google.maps.Geocoder() as Geocoder;
-
+    return this.geocoder$.pipe(
+      map(geocoder => {
         geocoder.geocode(request, (results, status) => {
           if (status === GeocoderStatus.OK) {
             return of(results);
