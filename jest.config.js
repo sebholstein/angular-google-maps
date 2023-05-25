7615111ea37cae4bd0c214774ac35416d1ffd5e4
kashgurung <b.bikashgurung@outlook.com>
2023-05-25 17:38:06 +0600

    fix: change the configuration for angular.json

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,9 @@
 module.exports = {
   preset: 'jest-preset-angular',
   //https://github.com/thymikee/jest-preset-angular/issues/167#issuecomment-459686655
-  // TODO: Only needed for @angular/cli libraries
-  // modulePathIgnorePatterns: ["<rootDir>/dist/"],
-  // moduleNameMapper: {
-  //   "^@agm/(.*)$": "<rootDir>/dist/$1",
-  // },
+  modulePathIgnorePatterns: ["<rootDir>/dist/"],
+  moduleNameMapper: {
+    "^@agm/(.*)$": "<rootDir>/dist/$1",
+  },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  globalSetup: 'jest-preset-angular/global-setup',
 };
